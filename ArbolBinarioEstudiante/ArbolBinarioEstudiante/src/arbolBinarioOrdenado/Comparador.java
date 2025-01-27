6801a74b68aecbf434320230862e654dfd848a44
Jhonny Romero <57994894+Joel3012@users.noreply.github.com>
2025-01-27 08:14:34 -0500

    Delete ArbolBinarioEstudiante/ArbolBinarioEstudiante directory

--- ArbolBinarioEstudiante/ArbolBinarioEstudiante/src/arbolBinarioOrdenado/Comparador.java
+++
@@ -1,9 +0,0 @@
-package arbolBinarioOrdenado;
-
-public interface Comparador {
-	boolean igualQue(Object q);
-	boolean menorQue(Object q);
-	boolean menorIgualQue(Object q);
-	boolean mayorQue(Object q);
-	boolean mayorIgualQue(Object q);
-}
